13249c330a69c2b665fbaec94dde0d1740fbd805
shivajikokare8007 <stk8007@gmail.com>
2022-07-12 20:25:59 +0530

    New java code

--- Hello.java
+++ Hello.java
@@ -1,17 +1,6 @@
-/**
- * 
- */
-package Demo1;
+public class Hello 
+{
 
-/**
- * @author Shivaji
- *
- */
-public class Hello {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!" + "\nThis is my first code.");
